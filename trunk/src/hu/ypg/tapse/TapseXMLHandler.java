ba41ef2f3a36e0b712799a42aeb7fbae0d38985f
balsa0 <ktibalazs@gmail.com>
2014-05-07 21:01:31 +0200

    Upgraded TapseXMLHandler class and settings xml.

--- trunk/src/hu/ypg/tapse/TapseXMLHandler.java
+++ trunk/src/hu/ypg/tapse/TapseXMLHandler.java
@@ -1,6 +1,7 @@
 package hu.ypg.tapse;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -11,8 +12,14 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 
 public class TapseXMLHandler {
+	
+	//logger
+	private static Logger LOGGER = Logger.getLogger("InfoLogging");
+	
+	//settings
 	private static boolean set_enabled;
 	private static Integer set_freq;
 	private static Integer id_lastShot;
@@ -27,13 +34,22 @@ public class TapseXMLHandler {
 	private static Document doc;
 	private static Element rootElement;
 	
-	//const settings params
+	//const settings tag name in xml
 	private static final String filePath = "res/settings.xml";
 	private static final String tr_root = "settings";
 	private static final String tr_enabled = "enabled";
 	private static final String tr_freq = "freq";
 	private static final String tr_lastID = "lastshotid";
+	private static final String tr_daysEnabled = "daysenabled";
+	private static final String tr_days = "days";
+	private static final String tr_from = "from";
+	private static final String tr_to = "to";
+	private static final String tr_progEnabled = "progenabled";
+	private static final String tr_prog = "prog";
 	
+	/**
+	 * Class constructor, initializes settings and starts xml parsing.
+	 */
 	public TapseXMLHandler(){
 		//initialize bool array
 		set_days = new boolean[7];
@@ -54,21 +70,30 @@ public class TapseXMLHandler {
 		}
 		
 		//initialize xml and variables
-		init();
+		initialize();
 	}
 	
-	public void init(){
+	/**
+	 * This method initializes XML parsing.
+	 */
+	public void initialize(){
+		
+		LOGGER.info("Reading XML...");
+		
 		try{
 			
+			//load xml file
 			File fXmlFile = new File(filePath);
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			doc = dBuilder.parse(fXmlFile);
-		 
+			
 			doc.getDocumentElement().normalize();
 			rootElement = doc.getDocumentElement();
 			String rootE = rootElement.getNodeName();
-			System.out.println("Root element :" + rootE);
+			
+			//logging root element
+			LOGGER.info("XML Root element found: "+rootE);
 		
 			//check root element
 			assert rootE == tr_root;
@@ -82,17 +107,13 @@ public class TapseXMLHandler {
 	}
 	
 	
-	//settings reader
+	/**
+	 * This method reads settings from 'settings.xml'.
+	 */
 	private void readSettings(){
 		
 		//getting enabled
-		String e = rootElement.getElementsByTagName(tr_enabled).item(0).getTextContent().toLowerCase().trim();
-		
-		if( e.equals("") || e.equals("true") ){
-			setEnabled(true);
-		}else{
-			setEnabled(false);
-		}
+		setEnabled(stringToBool(rootElement.getElementsByTagName(tr_enabled).item(0).getTextContent()));
 		
 		//getting frequency
 		setFrequency(Integer.parseInt(rootElement.getElementsByTagName(tr_freq).item(0).getTextContent()));
@@ -101,11 +122,38 @@ public class TapseXMLHandler {
 		setLastShotID(Integer.parseInt(rootElement.getElementsByTagName(tr_lastID).item(0).getTextContent()));
 	
 		//get days enabled
+		setDaysEnabled(stringToBool(rootElement.getElementsByTagName(tr_daysEnabled).item(0).getTextContent()));
 		
+		//get days of week enabled
+		NamedNodeMap dow = rootElement.getElementsByTagName(tr_days).item(0).getAttributes();
+		for (Integer i = 0; i < 7; i++){
+			String s = dow.getNamedItem("d"+i.toString()).getTextContent();
+			setDayOfWeekEnabled(i, stringToBool(s));
+		}
+		
+		//get time "from"
+		NamedNodeMap from = rootElement.getElementsByTagName(tr_from).item(0).getAttributes();
+		setTimeFrom(Integer.parseInt(from.getNamedItem("h").getTextContent()),
+					Integer.parseInt(from.getNamedItem("m").getTextContent()));
+		
+		//get time "to"
+		NamedNodeMap to = rootElement.getElementsByTagName(tr_to).item(0).getAttributes();
+		setTimeFrom(Integer.parseInt(to.getNamedItem("h").getTextContent()),
+					Integer.parseInt(to.getNamedItem("m").getTextContent()));
+		
+		//get program watching enabled
+		setProgEnabled(stringToBool(rootElement.getElementsByTagName(tr_progEnabled).item(0).getTextContent()));
+	
+		//get name of program to watch
+		setProgName(rootElement.getElementsByTagName(tr_prog).item(0).getTextContent());
 	}
 	
-	//setting writer
+	/**
+	 * This method writes changes to the settings XML.
+	 */
 	public static void settingsWriter(){
+		
+		//writing setting
 		try{
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
@@ -118,10 +166,19 @@ public class TapseXMLHandler {
 	}
 
 	//setters and getters
+	
+	/**
+	 * Returns shot frequency.
+	 * @return Returns shot frequency (in seconds)
+	 */
 	public Integer getFrequency() {
 		return set_freq;
 	}
 
+	/**
+	 * Sets shot frequency.
+	 * @param freq Shot frequency in seconds.
+	 */
 	public static void setFrequency(Integer freq) {
 		//freqency can't be smaller than '1'
 		if(freq < 1)
@@ -132,28 +189,39 @@ public class TapseXMLHandler {
 		rootElement.getElementsByTagName(tr_freq).item(0).setTextContent(set_freq.toString());
 	}
 
+	/**
+	 * Returns true if taking shots is allowed.
+	 * @return Returns true if taking shots is allowed.
+	 */
 	public static boolean isEnabled() {
 		return set_enabled;
 	}
 
+	/**
+	 * Sets program to active or passive state
+	 * @param enabled Enables or Disables taking shots.
+	 */
 	public static void setEnabled(boolean enabled) {
 		//set variable
 		set_enabled = enabled;
 		
 		//write xml
-		String r;
-		if(enabled)
-			r = "true";
-		else
-			r = "false";
-		
+		String r = boolToString(enabled);
 		rootElement.getElementsByTagName(tr_enabled).item(0).setTextContent(r);
 	}
 
+	/**
+	 * Returns the ID of the last shot.
+	 * @return Returns the ID of the last shot. (ID goes from 0 - )
+	 */
 	public static Integer getLastShotID() {
 		return id_lastShot;
 	}
 
+	/**
+	 * Sets the actual shot id minus one (act - 1)
+	 * @param num ID of the last shot.
+	 */
 	public static void setLastShotID(Integer num) {		
 		id_lastShot = num;
 		
@@ -161,21 +229,40 @@ public class TapseXMLHandler {
 		rootElement.getElementsByTagName(tr_lastID).item(0).setTextContent(id_lastShot.toString());
 	}
 	
+	/**
+	 * Returns true if scheduling shots is enabled.
+	 * @return Returns true if scheduling shots is enabled.
+	 */
 	public static boolean isDaysEnabled(){
 		return set_daysEnabled;
 	}
 	
+	/**
+	 * Enables/Disables scheduling shots.
+	 * @param enabled True turns scheduling on.
+	 */
 	public static void setDaysEnabled(boolean enabled){
 		set_daysEnabled = enabled;
 	}
 	
+	/**
+	 * Gets day of week and returns if shotting is enabled that day.
+	 * @param dow Day of week (0-6) Begins with Sonday.
+	 * @return Retrurns true if taking shots is enabled that day.
+	 */
 	public static boolean isDayOfWeekEnabed(int dow){
+		
 		//must be between 0 and 6
 		if(dow >= set_days.length || dow < 0)
 			return false;
 		return set_days[dow];
 	}
 	
+	/**
+	 * Enables/Disables taking shots on specific day.
+	 * @param dow Day of week (0-6) Begins with Sonday.
+	 * @param enabled True enables taking shots on the specific day.
+	 */
 	public static void setDayOfWeekEnabled(int dow, boolean enabled){
 		if(dow >= set_days.length || dow < 0)
 			return;
@@ -224,6 +311,23 @@ public class TapseXMLHandler {
 	
 	public static void setProgName(String name){
 		set_prog = name;
+	}
+	
+	//bool to string converter
+	public static final String boolToString(boolean b){
+		if(b)
+			return "true";
+		else
+			return "false";
+	}
+	
+	public static final boolean stringToBool(String s){
+		s = s.toLowerCase().trim();
+		if( s.equals("") || s.equals("true") ){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 }
